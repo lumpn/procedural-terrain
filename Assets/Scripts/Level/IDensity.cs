149432dfbee12a060b929cebe3cc15c0a1c61f7f
lumpn <lumpn@users.noreply.github.com>
2014-05-03 19:31:07 -0700

    Improved documentation.

--- Assets/Scripts/Level/IDensity.cs
+++ Assets/Scripts/Level/IDensity.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public interface IDensity {
 
-    /**
-     * Evaluates the given position to a density.
-     * Density ranges between [-1, 1] where 1 is solid and -1 is vacuum.
-     * The iso-surface where the density equals 0 is rendered.
-     */
+    /// <summary>
+    /// Evaluates the given position to a density.
+    /// Density ranges between [-1, 1] where 1 is solid and -1 is vacuum.
+    /// The iso-surface where the density equals 0 is rendered.
+    /// </summary>
     float Evaluate(Vector3 pos);
 }
